5726c1fa73f15c462adbf83433d01f21f83c77fa
Deng Sean <sean.deng@maersk.com>
2023-07-02 01:20:05 +0800

    lab-01

---
+++ lab-01-inital/src/main/java/com/example/lab01inital/Lab01InitalApplication.java
@@ -0,0 +1,13 @@
+package com.example.lab01inital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab01InitalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab01InitalApplication.class, args);
+    }
+
+}
